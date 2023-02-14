8a69cff2257fadef4094e3da9bae099b0360de0e
wangxiaoxin <wangxiaoxin@qq.com>
2023-02-14 15:17:44 +0800

    ======wxx branch commit=========

--- src/main/java/com/wuwei/demotest/DemotestApplication.java
+++ src/main/java/com/wuwei/demotest/DemotestApplication.java
@@ -12,12 +12,13 @@ public class DemotestApplication {
 
     public static void main(String[] args) {
         ConfigurableApplicationContext run = SpringApplication.run(DemotestApplication.class, args);
-
+   //  git log   git reflog
         UserProperties user = run.getBean("userProperties", UserProperties.class);
         System.out.println(user);
         System.out.println("git  add  后 又进行了修改");
         System.out.println("git  commit  后 又进行了修改");
         System.out.println("git  commit  后 又进行了提交");
+        System.out.println("git wxx分支提交");
     }
 
 }
