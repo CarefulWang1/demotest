a0db97f0bf88d2818a591d4a1ac4cd6d2d3f23af
wangxiaoxin <wangxiaoxin@qq.com>
2023-02-14 15:23:22 +0800

    ======wxx branch commit=========

--- src/main/java/com/wuwei/demotest/DemotestApplication.java
+++ src/main/java/com/wuwei/demotest/DemotestApplication.java
@@ -19,6 +19,7 @@ public class DemotestApplication {
         System.out.println("git  commit  后 又进行了修改");
         System.out.println("git  commit  后 又进行了提交");
         System.out.println("git wxx分支提交");
+        System.out.println("git 代码冲突  分支提交");
     }
 
 }
